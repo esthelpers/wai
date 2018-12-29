58f6a4595679a61201caaa81e9e8d12f27677766
Erdem Aybek <eaybek@gmail.com>
2018-12-29 17:14:30 +0300

    quick fix

--- est.sh
+++ est.sh
@@ -24,7 +24,7 @@ wai_source(){
          target=${${i%.*}#*.}
          if [[ $target == $CURRENT_SHELL ]]
          then
-             source $target
+             source $i
          fi
      done
     
